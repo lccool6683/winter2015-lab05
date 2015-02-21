74d99c914735bc2f9bd64efd7bcf51faf45afb02
Clemens Lo <lsps9140814@gmail.com>
2015-02-20 22:57:13 -0800

    randomly chosen quote Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,15 +18,13 @@ class Welcome extends Application {
     //  The normal pages
     //-------------------------------------------------------------
 
-    function index()
-    {
-	$this->data['pagebody'] = 'justone';    // this is the view we want shown
-	$this->data = array_merge($this->data, (array) $this->quotes->last());
+    function index() {
+        $this->data['pagebody'] = 'justone';    
+        $choice = rand(1, $this->quotes->size());
+        $this->data = array_merge($this->data, (array) $this->quotes->get($choice));
         $this->caboose->needed('jrating', 'hollywood');
         $this->data['average'] = ($this->data['vote_count'] > 0) ? ($this->data['vote_total'] / $this->data['vote_count']) : 0;
-	$this->render();
-    }
-
+        $this->render();
 }
 
 /* End of file Welcome.php */
