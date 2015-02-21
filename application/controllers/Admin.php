f73969be90163db0652be003880aad2a2762c8e2
Clemens Lo <lsps9140814@gmail.com>
2015-02-20 23:03:20 -0800

    Add quote_edit Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,17 +1,15 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 class Admin extends Application {
-
     function __construct() {
         parent::__construct();
+        // load the helper to display the form
+        $this->load->helper('formfields');
     }
-
     function index() {
         // use new admin_list to render the page
         $this->data['title'] = 'Quotations Maintenance';
@@ -19,5 +17,20 @@ class Admin extends Application {
         $this->data['pagebody'] = 'admin_list';
         $this->render();
     }
-
+    // Present a quotation for adding/editing
+    function present($quote) {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        // added the button with the formfileds helper
+        $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success');
+        $this->render();
+    }
+    // Add a new quotation
+    function add() {
+        $quote = $this->quotes->create();
+        $this->present($quote);
+    }
 }
